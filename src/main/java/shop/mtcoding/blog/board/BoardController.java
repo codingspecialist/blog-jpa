99a24529814900d67661734ac8db398d184c56e7
getinthere <getinthere@naver.com>
2025-04-16 11:01:51 +0900

    페이징완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.blog.user.User;
 
 @RequiredArgsConstructor
@@ -52,14 +49,16 @@ public class BoardController {
         return "board/detail";
     }
 
+    // localhost:8080?page=0
+    // localhost:8080
     @GetMapping("/")
-    public String list(HttpServletRequest request) {
+    public String list(HttpServletRequest request, @RequestParam(required = false, value = "page", defaultValue = "0") Integer page) {
         User sessionUser = (User) session.getAttribute("sessionUser");
 
         if (sessionUser == null) {
-            request.setAttribute("models", boardService.글목록보기(null));
+            request.setAttribute("model", boardService.글목록보기(null, page));
         } else {
-            request.setAttribute("models", boardService.글목록보기(sessionUser.getId()));
+            request.setAttribute("model", boardService.글목록보기(sessionUser.getId(), page));
         }
 
         return "board/list";
